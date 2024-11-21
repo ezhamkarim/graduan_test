1f2af1bb5fa6b30ef1516a2e66180d4160d1a03a
ezhamkarim <52437541+ezhamkarim@users.noreply.github.com>
2024-11-21 14:50:51 +0800

    add service

---
+++ lib/src/service/auth_service.dart
@@ -0,0 +1,21 @@
+class AuthService {
+  /// check auth
+  ///
+  ///
+  Future<String?> checkAuth() async {
+    return null;
+  }
+
+  /// login
+  ///
+  ///
+  Future<String?> login() async {
+    return null;
+  }
+
+  /// logout
+  ///
+  Future<String?> logout() async {
+    return null;
+  }
+}
